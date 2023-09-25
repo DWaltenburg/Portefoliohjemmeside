28ffb19891e4689e6102723e01ab94ddec3ad9d9
David Waltenburg <david@waltenburg.dk>
2023-09-25 12:43:55 +0200

    Added list filter via search field

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    array = [];
+
     $.ajax({
         url: "data.json",
         crossDomain: true,
@@ -7,7 +9,8 @@ $(document).ready(function () {
         },
         dataType: "json",
         success: function (data) {
-            console.log(data);
+            array = json2array(data);
+            showCards(data);
         },
         error: function (xhr, status) {
             console.log("error");
@@ -15,17 +18,33 @@ $(document).ready(function () {
     });
 
     $("#search").click(function () {
-        $("#my-list").append("<li>" + $("#searchTerm").val() + "</li>");
+        if ($("#searchTerm").val() == "") {
+            showCards(array);
+        } else {
+            const filteredData = array.filter(item => item.name.toLowerCase().includes($("#searchTerm").val().toLowerCase()));
+            console.log(filteredData);
+            showCards(filteredData);
+        }
     });
-    array = ["1", "2", "3", "4", "5", "6"]
+});
+function showCards(array) {
+    $(".row").empty();
     array.forEach(element => {
         $(".row").append('<div class="card col-md-2"\>' +
             '<img src = "Portrait_Placeholder.png" class= "card-img-top" alt = "Portrait">' +
             '<div class="card-body">' +
-            '<h5 class="card-title">Navn</h5>' +
-            '<p class="card-text">Beskrivelse</p>' +
+            '<h5 class="card-title">' + element.name + '</h5>' +
+            '<p class="card-text">' + element.description + '</p>' +
             '<a href="#" class="btn btn-primary">Gå til portefolio</a>' +
             '</div>' +
             '</div >')
     });
-});
+}
+function json2array(json) {
+    var result = [];
+    var keys = Object.keys(json);
+    keys.forEach(function (key) {
+        result.push(json[key]);
+    });
+    return result;
+}
